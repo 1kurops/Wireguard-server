965d7ff16f08f45e2645feb99fc5d119c40c0f2c
1kurops <hjvfffy@gmail.com>
2023-03-23 13:33:44 +0300

    update scripts

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
-Wireguard-server
+# Wireguard-server
 
 This project provides automatic deployment of a WireGuard VPN server depending on the server group. It includes the installation and initial setup of a master or slave VPN server, setting up the WireGuard unit, activating its auto-loading, enabling traffic forwarding, copying scripts from the repository for creating, deleting, updating, disabling, and enabling WireGuard configurations. It also includes copying the Prometheus exporter for WireGuard and its setup and activation.
-Technologies
 
-WireGuard, Bash, Prometheus exporter.
-Installation
+The project involves using the WireGuard Exporter for Prometheus, developed by MindFlavor, for monitoring purposes. The scripts used in the project have an added feature that utilizes the "friendly_name" parameter of the exporter, making it easier to monitor the consumption and usage of each configuration. 
+
+### Technologies
+
+`WireGuard, Bash.`
+
+
+## Installation
 
 To install, run the following command with root privileges:
 ```
-sudo curl -o- https://raw.githubusercontent.com/1kurops/Wireguard-server/main/installwg-server.sh | bash
+sudo curl -o- https://raw.githubusercontent.com/1kurops/wireguard-server/main/installwg-server.sh | bash
 ```
 
-Configuration
+## Configuration
 
 This configuration uses the /10 subnet mask for the internal subnet, which means you will have access to generating 4,194,304 internal IP addresses. Thus, the maximum number of configurations is 4,194,304.
 
-To manage WireGuard configurations, use the following scripts located in `/etc/wireguard/scripts/`:
+### To manage WireGuard configurations, use the following scripts located in `/etc/wireguard/scripts/`:
 
   `wg_make.sh`: creates a new configuration. This script also supports arguments. For example, running wg_make.sh conf_name will create a configuration with the name "conf_name."
 
@@ -30,9 +35,11 @@ To manage WireGuard configurations, use the following scripts located in `/etc/w
 Additionally, there are several service scripts that are active by default:
 
   `backup.sh`: creates an incremental backup of the WireGuard directory. Copies of backups older than 14 days are automatically deleted.
+  
   `banscript.sh`: tracks traffic usage and, if it exceeds a certain limit (10GB by default), disables the configuration until the next day.
+  
   `clientcheck.sh`: tracks unused configurations and writes their names to the file /opt/wireguard_server/usercheck.txt.
 
-Contributors
+### Contributors
 
-    1kurops
+1kurops
--- installwg-server.sh
+++ installwg-server.sh
@@ -5,11 +5,9 @@
 #                      #
 ########################
 
-
-
 if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root" >&2
-    exit 1
+  echo "This script must be run as root" >&2
+  exit 1
 fi
 
 apt-get update
@@ -18,98 +16,95 @@ apt-get --yes --no-install-recommends install git
 apt-get --yes --no-install-recommends install wireguard
 apt-get --yes --no-install-recommends install rsync
 apt-get --yes --no-install-recommends install cron
-WgUrl="https://github.com/1kurops/Wireguard-server.git"
+WgUrl="https://github.com/1kurops/wireguard-server.git"
 cd /opt/
 echo "cloning reporitory"
 
-  git clone $WgUrl
-
-touch /opt/wireguard_server/wgset.conf
-wgset="/opt/wireguard_server/wgset.conf"
+git clone $WgUrl
+touch /opt/wireguard-server/wgset.conf
+wgset="/opt/wireguard-server/wgset.conf"
+touch /etc/wireguard/wg0.conf
 wgconf="/etc/wireguard/wg0.conf"
 
-
-function setup_master_server_info () {
+function setup_master_server_info() {
   IP_MAIN_SERV="$(dig +short myip.opendns.com @208.67.222.222)"
   echo "IP addres server $IP_MAIN_SERV"
   echo -n "Please enter the domain name: "
-  read -r domain_name  }
+  read -r domain_name
+}
 
-
-function setup_slave_server_info () {
+function setup_slave_server_info() {
   echo -n "Please enter the ipAddress slave server: "
   read -r IP_SLAVE_SERV
   echo -n "Please enter the domain name: "
-  read -r domain_name  }
+  read -r domain_name
+}
 
-
-function verify_domainname () {
-      ip_address=$(ping -c 1 -n 1 -4 $domain_name | grep -Eo --max-count=1 '([0-9]{1,3}\.){3}[0-9]{1,3}')
+function verify_domainname() {
+  if [ -z "$domain_name" ]; then
+    echo "error domain name, be used IPaddres"
+    IPv4pub="$(dig +short myip.opendns.com @208.67.222.222)"
+  else
     if [ -z "$ip_address" ]; then
       echo "error domain name, be used IPaddres"
       IPv4pub="$(dig +short myip.opendns.com @208.67.222.222)"
     else
       echo "domain name corrert"
       IPv4pub=$domain_name
-    fi  }
+    fi
+  fi
+}
 
 function master_slave_property() {
   echo -n "Specify if this is a master or slave server (m/s): "
   read -r master_slave
-  
+
   if [ -z "$master_slave" ]; then
     echo "Please enter m or s"
     master_slave_property
   elif [ "$master_slave" != "m" ] && [ "$master_slave" != "s" ]; then
     echo "Invalid input. Please enter m or s"
     master_slave_property
-  fi  }
+  fi
+}
 
-function slave_availability () {
+function slave_availability() {
   echo -n "do you have slave server ? (y/n): "
   read -r slave_true
-    if [ -z "$slave_true" ]; then
+  if [ -z "$slave_true" ]; then
     echo "Please enter y or n"
-    elif [ "$slave_true" != "m" ] && [ "$slave_true" != "s" ]; then
+  elif [ "$slave_true" != "y" ] && [ "$slave_true" != "n" ]; then
     echo "Invalid input. Please enter y or n"
     slave_availability
-    fi  }
+  elif [ "$slave_true" != "n" ]; then
+    setup_slave_server_info
+  else
+    setup_master_server_info
+  fi
+}
+echo -n "Please enter the port vpn server (default 51820): "
+read -r port
+if [ -z "$port" ]; then
+  wgPORT="51820"
+else
+  wgPORT="$port"
+fi
+echo -n "Please enter the DNS (default 1.1.1.1): "
+read -r DNS
+if [ -z "$DNS" ]; then
+  wgDNS1="1.1.1.1"
+  wgDNS2="1.0.0.1"
+else
+  wgDNS1=$DNS
+fi
+echo -n "Please enter the MTU (default 1420): "
+read -r MTU
+if [ -z "$MTU" ]; then
+  wgMTU="1420"
+else
+  wgMTU="$MTU"
+fi
 
-  function monitoring () {
-  echo -n "Do you want to install wireguard exporter for monitoring server (y/n): "
-  read -r monitoring_conf
-  
-  if [ -z "$monitoring_conf" ]; then
-    echo "Please enter y or n"
-    monitoring
-  elif [ "$monitoring_conf" != "m" ] && [ "$monitoring_conf" != "s" ]; then
-    echo "Invalid input. Please enter m or s"
-    monitoring
-  fi  }
-
-
-  echo -n "Please enter the port vpn server (default 51820): "
-  read -r port
-    if [ -z "$port" ]; then
-      wgPORT="51820"
-    else
-      wgPORT="$port"
-    fi
-  echo -n "Please enter the DNS (default 1.1.1.1): "
-  read -r DNS
-    if [ -z "$DNS" ]; then
-      wgDNS1="1.1.1.1"
-      wgDNS2="1.0.0.1"
-    else
-      wgDNS1=$DNS
-    fi
-  echo -n "Please enter the MTU (default 1420): "
-  read -r MTU
-    if [ -z "$MTU" ]; then
-      wgMTU="1420"
-    else
-      wgMTU="$MTU"
-    fi
 {
   echo "wgNET=10.64.0.1"
   echo "subnetClass=10"
@@ -119,77 +114,79 @@ function slave_availability () {
   echo "wgDNS1=$wgDNS1"
   echo "wgDNS2=$wgDNS2"
   echo "wgMTU=$wgMTU"
-} >> $wgset
+} >>$wgset
 
 master_slave_property
-  if [ $master_slave == "m" ]; then
-   slave_availability
-    if [ $slave_true == "y" ]; then
-      setup_slave_server_info
-        verify_domainname "$domain_name"
-        cd /root/.ssh/
-        {
-          echo "Host slave"
-          echo "HostName $IP_SLAVE_SERV"
-          echo "Port 22"
-          echo "User root"
-          echo "IdentityFile ~/.ssh/$name"
-          echo "IdentitiesOnly yes"
-          echo "wgDEV=wg0"
-          } >> config
-          echo "IpSlave=$IP_SLAVE_SERV" >> $wgset
-    fi
+if [ $master_slave == "m" ]; then
+  slave_availability
+  if [ $slave_true == "y" ]; then
+    setup_slave_server_info
+    verify_domainname "$domain_name"
+    cd /root/.ssh/
+    {
+      echo "Host slave"
+      echo "HostName $IP_SLAVE_SERV"
+      echo "Port 22"
+      echo "User root"
+      echo "IdentityFile ~/.ssh/$name"
+      echo "IdentitiesOnly yes"
+      echo "wgDEV=wg0"
+    } >>config
+    echo "IpSlave=$IP_SLAVE_SERV" >>$wgset
   else
     echo "Setup main server"
-      setup_master_server_info 
-        verify_domainname "$domain_name"  
-    echo "wgHOST=$IPv4pub" >> $wgset
-  fi 
+    setup_master_server_info
+    verify_domainname "$domain_name"
+    echo "wgHOST=$IPv4pub" >>$wgset
+  fi
+fi
 
 echo "Setup available Interface"
 availableInterfaces="$(echo "$(ip -o link)" | awk '/state UP/ {print $2}')"
-availableInterfaces="$(echo "${availableInterfaces}" \
-    | cut -d ':' -f 1 \
-    | cut -d '@' -f 1 \
-    | grep -v -w 'lo' \
-    | grep -v '^docker')"
-    echo "IPv4dev=$availableInterfaces" >> $wgset
-    IPv4dev=$availableInterfaces
+availableInterfaces="$(echo "${availableInterfaces}" |
+  cut -d ':' -f 1 |
+  cut -d '@' -f 1 |
+  grep -v -w 'lo' |
+  grep -v '^docker' |
+  head -1)"
+echo "IPv4dev=$availableInterfaces" >>$wgset
+IPv4dev=$availableInterfaces
 
 cp -r /opt/wireguard-server/scripts/ /etc/wireguard/
 mkdir /etc/wireguard/configs
 mkdir /etc/wireguard/keys
 
 source "${wgset}"
-  wg genkey \
-    | tee /etc/wireguard/keys/server_priv &> /dev/null
-      cat /etc/wireguard/keys/server_priv \
-    | wg pubkey \
-    | tee /etc/wireguard/keys/server_pub &> /dev/null
+wg genkey |
+  tee /etc/wireguard/keys/server_priv &>/dev/null
+cat /etc/wireguard/keys/server_priv |
+  wg pubkey |
+  tee /etc/wireguard/keys/server_pub &>/dev/null
 
 echo "Server Keys have been generated."
-    {
-    echo '[Interface]'
-    echo "PrivateKey = $(cat /etc/wireguard/keys/server_priv)"
-    echo "Address = ${wgNET}/${subnetClass}"
-    echo "MTU = ${wgMTU}"
-    echo "ListenPort = ${wgPORT}"
-    echo "PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $IPv4dev -j MASQUERADE"
-    echo "PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $IPv4dev -j MASQUERADE"
+{
+  echo '[Interface]'
+  echo "PrivateKey = $(cat /etc/wireguard/keys/server_priv)"
+  echo "Address = ${wgNET}/${subnetClass}"
+  echo "MTU = ${wgMTU}"
+  echo "ListenPort = ${wgPORT}"
+  echo "PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $IPv4dev -j MASQUERADE"
+  echo "PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $IPv4dev -j MASQUERADE"
 
-    } | tee /etc/wireguard/wg0.conf &> /dev/null
+} | tee /etc/wireguard/wg0.conf &>/dev/null
 echo "Enable Forwarding"
 forward="net.ipv4.ip_forward=1"
-    if ! grep -q "$forward" /etc/sysctl.conf; then
-      echo "$forward" >> /etc/sysctl.conf
-    else
-      sed -i "s|#net.ipv4.ip_forward=1|${forward}|" /etc/sysctl.conf
-    fi
-echo 'net.ipv4.ip_forward=1' \
-    | tee /etc/sysctl.d/99-wireguard.conf > /dev/null
+if ! grep -q "$forward" /etc/sysctl.conf; then
+  echo "$forward" >>/etc/sysctl.conf
+else
+  sed -i "s|#net.ipv4.ip_forward=1|${forward}|" /etc/sysctl.conf
+fi
+echo 'net.ipv4.ip_forward=1' |
+  tee /etc/sysctl.d/99-wireguard.conf >/dev/null
 sysctl -p
 
-cat /etc/wireguard/scripts/unit.conf > /etc/systemd/system/wg-quick@.service
+cat /etc/wireguard/scripts/wg-quick@.service >/etc/systemd/system/wg-quick@.service
+rm /etc/wireguard/scripts/wg-quick@.service
 systemctl daemon-reload
 systemctl start wg-quick@wg0.service
 systemctl enable wg-quick@wg0.service
@@ -201,44 +198,13 @@ systemctl enable wg-quick@wg0.service
   echo "2 2 * * * root bash /etc/wireguard/scripts/wg_enable.sh -ub &> /dev/null"
   echo "1 1 * * * root bash /etc/wireguard/scripts/usercheck.sh &> /dev/null"
   echo "5 2 * * * root wg-quick down wg0; systemctl restart wg-quick@wg0.service"
-  echo "* * * * * root systemctl reload wg-quick@wg0.service"  } >> /etc/crontab
+  echo "* * * * * root systemctl reload wg-quick@wg0.service"
+} >>/etc/crontab
 
 chmod 700 /etc/wireguard/
 chmod +x /etc/wireguard/scripts/*.sh
+mv $wgset /etc/wireguard/scripts/
+export PATH=$PATH:/etc/wireguard/scripts
+source ~/.bashrc
 
 echo "Installation Complete!"
-
-monitoring
-if [ $monitoring_conf == "n" ]; then 
- echo "Installation Complete!"
-else
- echo "Installation wireguard exporter"
- cd /opt/wireguard_server/
- chmod +x prometheus_wireguard_exporter
- {
-  echo "[Unit]"
-  echo "Description=Prometheus WireGuard Exporter https://github.com/MindFlavor/prometheus_wireguard_exporter"
-  echo "Wants=network-online.target"
-  echo "After=network-online.target"
-
-  echo "[Service]"
-  echo "User=root"
-  echo "Group=root"
-  echo "Type=simple"
-  echo "ExecStart=/opt/wg_exporter/prometheus_wireguard_exporter -n $wgconf -i $wgDEV"
-
-  echo "[Install]"
-  echo "WantedBy=multi-user.target"
- } > wg_exporter.service
-
- mv wg_exporter.service /etc/systemd/system/
- systemctl daemon-reload
- systemctl start wg_exporter.service && systemctl enable wg_exporter.service
- echo "Start the binary with -h to get the complete syntax. " 
- echo "Once started, the tool will listen on the specified port (or the default one, 9586, if not specified)"
- echo "and return a Prometheus valid response at the url /metrics. So to check if the tool is working properly simply "
- echo "browse the http://localhost:9586/metrics (or whichever port you choose)."
- 
- echo 
- echo "Installation Complete!"
-fi
